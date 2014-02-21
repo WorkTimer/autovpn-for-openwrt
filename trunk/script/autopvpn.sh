4cda7416e68290981899f2cc20369d35fc7426e5
autovpn2014@gmail.com <autovpn2014@gmail.com@d33a5e55-9933-2082-6c48-4aa3451dfce2>
2014-02-21 09:42:14 +0000

    improve script

--- trunk/script/autopvpn.sh
+++ trunk/script/autopvpn.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-. /etc/autovpn.conf
 ps |grep -v grep|grep -qe "ssh.*-TCf\|pppd ifname pvpn"
 if [ $? -ne 0 ] ;then
+    . /etc/autovpn.conf
     echo "[ `date +'%Y/%m/%d %H:%M:%S'` ] restart pvpn" >>/tmp/autopvpn.log
     export SSH_ARGS="-p $SSH_PORT -i $SSH_KEY"
-    pvpn root@$SSH_SERVER 8.8.8.8
+    pvpn -r "$ROUTES" root@$SSH_SERVER 8.8.8.8
     kill -HUP `pgrep dnsmasq`
-    while read route ; do
-        ip route add $route
-    done 2>/dev/null <$ROUTES
 fi
--- trunk/script/routeadd.sh
+++ trunk/script/routeadd.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-DEV=`ifconfig |grep -e "tun[0-9]\|pvpn"|cut -d" " -f 1`
 IPS=`echo $1|tr "," "\n"`
 
 if [ -n "$IPS" ] ;then
@@ -9,7 +8,7 @@ if [ -n "$IPS" ] ;then
         if [ $? -ne 0 ];then
             echo "add $ip to route table"
             ip route add $ip dev $DEV
-            echo "$ip dev $DEV" >>$ROUTES
+            echo "$ip" >>$ROUTES
         fi
     done
 fi
